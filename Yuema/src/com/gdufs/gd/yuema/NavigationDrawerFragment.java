bf1ebfdaf9a31c3af32730d56005d96bc259a821
kelink <luokelin1992724@qq.com>
2015-03-03 22:25:23 +0800

    init

--- Yuema/src/com/gdufs/gd/yuema/NavigationDrawerFragment.java
+++ Yuema/src/com/gdufs/gd/yuema/NavigationDrawerFragment.java
@@ -1,5 +1,6 @@
 package com.gdufs.gd.yuema;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -9,8 +10,6 @@ import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
